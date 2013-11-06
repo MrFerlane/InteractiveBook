a22a3dbe87ef367dc253e5db27cdbe562e5a523b
azhelt <azhelt@railsServer.(none)>
2013-11-06 18:26:38 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Booksite::Application.config.secret_key_base = 'a64239005e6359d1d6a0330c0a95eb60fabf36fdb6708d8e5b0d8b3a54dd7854f9bbd1a7ce033eeca735b9295f8736ab781937d1cd4be4886aa10ed0a0b96213'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Booksite::Application.config.session_store :cookie_store, key: '_Booksite_session'
